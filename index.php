108c315f5d9b65dd06946a117512cb29a60826ad
babu tech <babutech@babus-MacBook-Air.local>
2023-05-23 12:35:20 +0530

    commencing word with wordpress theme  developement

---
+++ index.php
@@ -0,0 +1,19 @@
+Welcome to Designer Storm Theme !
+<br/>
+<?php while(have_posts()){
+    the_post();?>
+    <h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+    <?php the_content(); ?>
+    <hr/>
+<?php }
+?>
+
+
+
+
+
+
+
+<?php bloginfo('name'); ?>
+<br/>
+<?php bloginfo('description'); ?>
